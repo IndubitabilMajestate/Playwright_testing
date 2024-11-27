3d5280447adfd96a5ba84ff7dfb46c0d3a62c5aa
Razvan Popa <razvan.a.popa@dxc.com>
2024-11-27 14:22:09 +0200

    WIP commit 6

--- Form.py
+++ Form.py
@@ -8,21 +8,38 @@ class Form:
         if data is None:
             data = {}
         form_locator = self.page.locator(f"xpath={xpath}//form")
-        form_field_wrappers = form_locator.locator("xpath=.//div[(.//input or .//textarea or .//select or .//button) and .//label]")
+        form_field_wrappers = form_locator.locator("xpath=.//div[contains(translate(@id,'W','w'),'wrapper')]")
         for form_field_wrapper in form_field_wrappers.all():
             wrapper_label = form_field_wrapper.locator("xpath=.//label").text_content()
             # print(wrapper_label)
             # print(form_field_wrapper.locator("xpath=//input").count())
-            if form_field_wrapper.locator("xpath=//input").count() > 0:
-                input_locator = form_field_wrapper.locator("xpath=//input")
-                self.fillInputorTextarea(data[wrapper_label], "", input_locator)
-            elif form_field_wrapper.locator("xpath=//textarea").count() > 0:
-                input_locator = form_field_wrapper.locator("xpath=.//")
-                self.fillInputorTextarea(data[wrapper_label], "", input_locator)
-            elif form_field_wrapper.locator("xpath=//select").count() > 0:
-                pass
-            elif form_field_wrapper.locator("xpath=//button").count() > 0:
-                pass
+            input_locators = form_field_wrapper.locator("xpath=.//input | .//textarea | .//select | .//button")
+            if input_locators.count() > 1:
+                for input_locator in input_locators.all():
+                    input_type = input_locator.get_attribute("type")
+                    tag_type = input_locator.get_attribute("tagName").lower()
+                    if input_type == 'text' or tag_type == "textarea":
+                        key_name = input_locator.get_attribute('placeholder') if input_locator.get_attribute('placeholder') else (
+                            input_locator.get_attribute('value'))
+                        self.fillInputorTextarea(data[wrapper_label][key_name], "", input_locator)
+                    elif form_field_wrapper.locator("xpath=//textarea").count() > 0:
+                        input_locator = form_field_wrapper.locator("xpath=.//")
+                        self.fillInputorTextarea(data[wrapper_label], "", input_locator)
+                    elif form_field_wrapper.locator("xpath=//select").count() > 0:
+                        pass
+                    elif form_field_wrapper.locator("xpath=//button").count() > 0:
+                        pass
+            else:
+                if form_field_wrapper.locator("xpath=//input").count() > 0:
+                    input_locator = form_field_wrapper.locator("xpath=.//input")
+                    self.fillInputorTextarea(data[wrapper_label], "", input_locator)
+                elif form_field_wrapper.locator("xpath=//textarea").count() > 0:
+                    input_locator = form_field_wrapper.locator("xpath=.//")
+                    self.fillInputorTextarea(data[wrapper_label], "", input_locator)
+                elif form_field_wrapper.locator("xpath=//select").count() > 0:
+                    pass
+                elif form_field_wrapper.locator("xpath=//button").count() > 0:
+                    pass
 
     def fillInputorTextarea(self,text,xpath,parent=None):
         input_locator = parent.locator(f"xpath={xpath}//.") if parent is not None else self.page.locator(f"xpath={xpath}//.")
---
+++ Task6.py
@@ -0,0 +1,23 @@
+from random import random
+from playwright.sync_api import sync_playwright
+from Utils import navigateToCategory
+from Form import Form
+
+def Task6():
+    with sync_playwright() as playwright:
+        page = navigateToCategory(playwright.chromium.launch(headless=False).new_page(),'Forms','Practice Form')
+        data = {
+            'Name' : {'First Name': f'FirstName{round(10*random())}', 'Last Name': f'LastName{round(10*random())}'},
+            'Email' : f'email{round(10*random())}@test.com',
+            'Gender' : 'Male' if round(3*random()) == 0 else 'Female' if round(3*random()) == 1 else 'Other',
+            'Mobile' : f'{round(9999999999*random())}',
+            'Date of Birth': f'{round(28*random())}.{round(12*random())}.19{50+round(49*random())}',
+            'Subjects' : 'English\nMaths\n', ## not ok
+            'Hobbies': [1, 3],
+            'Current Address': f'Street no.{round(100*random())}'
+        }
+
+        form_locator = Form(page,data)
+
+if __name__ == '__main__':
+    Task6()
